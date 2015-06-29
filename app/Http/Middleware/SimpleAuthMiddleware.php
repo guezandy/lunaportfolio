3f43ba7373fb75014d3e7529d6b9c7eeeb714034
Andrew Rodriguez <andrew.rodriguez007@gmail.com>
2015-06-29 15:29:32 -0500

    no username->name

--- app/Http/Middleware/SimpleAuthMiddleware.php
+++ app/Http/Middleware/SimpleAuthMiddleware.php
@@ -16,6 +16,6 @@ class SimpleAuthMiddleware
      */
     public function handle($request, Closure $next)
     {
-        return Auth::onceBasic('username') ?: $next($request); //$next($request);
+        return Auth::onceBasic('name') ?: $next($request); //$next($request);
     }
 }
